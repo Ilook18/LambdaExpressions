8289c72711b79ce0321b317baae7d31e3eb8e520
“Enter <anirudhanand984@gmail.com>
2021-12-12 12:12:59 +0530

    [Anirudh] add : Lambda Expressions

---
+++ LambdaExpressions/LambdaExpressions/PersonDataManagement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LambdaExpressions
+{
+    class PersonDataManagement
+    {
+        public static void AddRecords(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person("283456876", "John", "12 Main street,Newyork,NY", 15));
+            listPersonInCity.Add(new Person("283456877", "SAM", "13 Main street,Newyork,NY", 25));
+            listPersonInCity.Add(new Person("283456878", "Elen", "14 Main street,Newyork,NY", 35));
+            listPersonInCity.Add(new Person("283456879", "Albert", "15 Main street,Newyork,NY", 45));
+            listPersonInCity.Add(new Person("283456880", "Peter", "16 Main street,Newyork,NY", 55));
+            listPersonInCity.Add(new Person("283456881", "Jack", "17 Main street,Newyork,NY", 65));
+           
+            foreach(var item in listPersonInCity)
+            {
+                Console.WriteLine("Name : " + item.SSN + "\t\tName: " + item.Name + "\t\tAddress" + item.Address + "\t\tAge" + item.Age + "\n");
+            }
+
+
+        }
+       
+        }
+        }
+
+    
+
---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,32 @@
+﻿using LambdaExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> listPersonInCity = new List<Person>();
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE OPTION:\n1.Add Records\n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        PersonDataManagement.AddRecords(listPersonInCity);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
