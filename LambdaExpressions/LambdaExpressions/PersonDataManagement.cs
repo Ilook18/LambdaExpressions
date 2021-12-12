2a973f79b19505ce2bd053bdac17730fd460c691
“Enter <anirudhanand984@gmail.com>
2021-12-12 12:41:04 +0530

    [Anirudh] add : Records Of people with age less than Sixty

--- LambdaExpressions/LambdaExpressions/PersonDataManagement.cs
+++ LambdaExpressions/LambdaExpressions/PersonDataManagement.cs
@@ -15,17 +15,26 @@ namespace LambdaExpressions
             listPersonInCity.Add(new Person("283456879", "Albert", "15 Main street,Newyork,NY", 45));
             listPersonInCity.Add(new Person("283456880", "Peter", "16 Main street,Newyork,NY", 55));
             listPersonInCity.Add(new Person("283456881", "Jack", "17 Main street,Newyork,NY", 65));
-           
-            foreach(var item in listPersonInCity)
+
+            foreach (var item in listPersonInCity)
             {
                 Console.WriteLine("Name : " + item.SSN + "\t\tName: " + item.Name + "\t\tAddress" + item.Address + "\t\tAge" + item.Age + "\n");
             }
+        }
 
+        public static void Retreiving_TopTwoRecords_ForAgeIs_LessThanSixty(List<Person> listPersonInCity)
+        {
+            foreach (Person person in listPersonInCity.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.Write("Name : " + person.Name + "\t\tAge: " + person.Age + "\n");
+            }
 
         }
-       
-        }
-        }
+    }
+}
+
+    
+        
 
     
 
--- LambdaExpressions/LambdaExpressions/Program.cs
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -14,7 +14,7 @@ namespace LambdaExpressions
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("CHOOSE OPTION:\n1.Add Records\n2.Exit");
+                Console.WriteLine("CHOOSE OPTION:\n1.Add Records\n2.Retreiving Records For age less than Sixty\n3.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -22,6 +22,9 @@ namespace LambdaExpressions
                         PersonDataManagement.AddRecords(listPersonInCity);
                         break;
                     case 2:
+                        PersonDataManagement.Retreiving_TopTwoRecords_ForAgeIs_LessThanSixty(listPersonInCity);
+                        break;
+                    case 3:
                         flag = false;
                         break;
 
